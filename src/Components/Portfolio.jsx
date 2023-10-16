bafc1d239a11f1456d0dceef7bcf84c7f34f72c5
david murage <110826121+davidmurage@users.noreply.github.com>
2023-10-16 17:27:36 +0300

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -33,13 +33,13 @@ const projectList = [
   {
     title: "10 Things To Know About Azure Static Web Apps 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
+      "Collaboration to create a beginner friendly article to help explain Azure Static Web Application and tooling to get started.",
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
     title: "Web Development for Beginners",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being demonstrated/taught.",
     url: "https://github.com/microsoft/web-dev-for-beginners",
   },
   {
